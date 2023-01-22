2c577fe26da1fabffe892d84102279ed7af0d509
kenwhitestone2 <sashisematte@gmail.com>
2023-01-22 17:18:58 -0600

    walking

--- AstroSurvivor/Assets/scripts/main_player/walking.cs
+++ AstroSurvivor/Assets/scripts/main_player/walking.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class walking : MonoBehaviour
 
 {
-    public Rigidbody2D rbPlayer;
-
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("oops");
+        rb = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
-            rbPlayer.AddForce(new Vector2(5f, 0f));
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = (mousePos - transform.position).normalized;
+
+            rb.velocity = Vector2.zero;
+            rb.AddForce(direction * moveSpeed, ForceMode2D.Impulse);
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            rb.velocity = Vector2.zero;
         }
     }
 
- }
+}
