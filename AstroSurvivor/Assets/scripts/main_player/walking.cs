478e70872fc06dc0324d2f54cec8154b76f0e68d
kenwhitestone2 <sashisematte@gmail.com>
2023-01-21 20:47:11 -0600

    movement test

--- AstroSurvivor/Assets/scripts/main_player/walking.cs
+++ AstroSurvivor/Assets/scripts/main_player/walking.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class walking : MonoBehaviour
+
 {
+    public Rigidbody2D rbPlayer;
+
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("oops");
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) {
+            rbPlayer.AddForce(new Vector2(5f, 0f));
+        }
     }
-}
+
+ }
